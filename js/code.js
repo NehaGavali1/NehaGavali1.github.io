5579b15766bdbd363f0e6931b3da9302d7a88351
NehaGavali1 <88278891+NehaGavali1@users.noreply.github.com>
2022-10-12 17:04:45 +0530

    hello

---
+++ js/code.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $(".navigation .navbar .logo-toggle-container .toggle-box").click(function(){
+//         $(".navigation .menu").stop().slideToggle();
+//     });
+// });
+{/* <img src="../images/bag.jpg" style="width:210px;"></img>
+<img src="../images/shoes.jpg" style="width:210px;"></img>
+<img src="../images/scandal.webp" style="width:210px;"></img>
+<img src="../images/jacket.webp"style="width:210px;">
+<img src="../images/insert_jacket.webp"style="width:210px;"></img> */}
